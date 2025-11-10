a8f34319ac988bc84da15c06881830fa03f97fef
sucharita4867 <sucharitasardar7@gmail.com>
2025-11-10 22:59:15 +0530

    Added user creation and onAuthStateChanged functionality in Register page

--- src/Pages/Register.jsx
+++ src/Pages/Register.jsx
@@ -1,67 +1,79 @@
-import React from "react";
+import React, { use } from "react";
 import { FcGoogle } from "react-icons/fc";
 import { Link } from "react-router";
+import { AuthContext } from "../Provider/AuthProvider";
 
 const Register = () => {
+  const { createUser, setUser } = use(AuthContext);
   const handelRegister = (e) => {
     e.preventDefault();
     const name = e.target.name.value;
     const email = e.target.email.value;
     const photo = e.target.photo.value;
     const password = e.target.password.value;
-    console.log(name, email, photo, password);
+    console.log({ name, email, photo, password });
+    createUser(email, password)
+      .then((result) => {
+        const user = result.user;
+        setUser(user);
+        // console.log(user);
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        alert(errorMessage);
+      });
   };
   return (
     <div className="text-black flex justify-center items-center my-4">
       <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl border border-black">
-        <div className="card-body">
-          <h1 className="text-[#464646] poppins text-center font-semibold text-2xl ">
-            Please Register!
-          </h1>
-          <fieldset className="fieldset">
-            <form onSubmit={handelRegister}>
-              {/* name */}
-              <label className="label">Name</label>
-              <input
-                name="name"
-                type="text"
-                className="rounded-full input"
-                placeholder="Name"
-                required
-              />
-              {/* email */}
-              <label className="label">Email</label>
-              <input
-                name="email"
-                type="email"
-                className="rounded-full input"
-                placeholder="Email"
-                required
-              />
-              {/* photo */}
-
-              <label className="label">Photo URL </label>
-              <input
-                name="photo"
-                type="text"
-                className="rounded-full input"
-                placeholder="Photo URl"
-                required
-              />
-              {/* password */}
-              <label className="label">Password</label>
-              <input
-                name="password"
-                type="password"
-                className="input rounded-full"
-                placeholder="Password"
-                required
-              />
-
-              <button className="btn w-full mt-4 md:px-8 text-center bg-[#F8B864] rounded-full text-base text-white md:font-semibold hover:border hover:border-[#F8B864] hover:bg-[white] hover:text-[#F8B864]">
-                Login
-              </button>
-            </form>
+        <h1 className="text-[#464646] poppins text-center font-semibold text-2xl ">
+          Please Register!
+        </h1>
+        <form onSubmit={handelRegister} className="card-body">
+          <fieldset className="fieldset ">
+            {/* name */}
+            <label className="label">Name</label>
+            <input
+              name="name"
+              type="text"
+              className="rounded-full input"
+              placeholder="Name"
+              required
+            />
+            {/* email */}
+            <label className="label mt-2">Email</label>
+            <input
+              name="email"
+              type="email"
+              className="rounded-full input"
+              placeholder="Email"
+              required
+            />
+            {/* photo */}
+            <label className="label mt-2">Photo URL </label>
+            <input
+              name="photo"
+              type="text"
+              className="rounded-full input"
+              placeholder="Photo URl"
+              required
+            />
+            {/* password */}
+            <label className="label mt-2">Password</label>
+            <input
+              name="password"
+              type="password"
+              className="input rounded-full"
+              placeholder="Password"
+              required
+            />
+            <button
+              type="submit"
+              className="btn w-full mt-4 md:px-8 text-center bg-[#F8B864] rounded-full text-base text-white md:font-semibold hover:border hover:border-[#F8B864] hover:bg-[white] hover:text-[#F8B864]"
+            >
+              Register
+            </button>
             <button className="btn bg-white text-black  rounded-full border border-[#F8B864]">
               <FcGoogle className="text-xl" />
               Login with Google
@@ -74,7 +86,7 @@ const Register = () => {
               </span>
             </p>
           </fieldset>
-        </div>
+        </form>
       </div>
     </div>
   );
--- src/Provider/AuthProvider.jsx
+++ src/Provider/AuthProvider.jsx
@@ -1,13 +1,35 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useEffect, useState } from "react";
+import app from "../Firebase/firebase.config";
+import {
+  createUserWithEmailAndPassword,
+  getAuth,
+  onAuthStateChanged,
+} from "firebase/auth";
 
+const auth = getAuth(app);
 export const AuthContext = createContext();
 const AuthProvider = ({ children }) => {
-  const [user, setUser] = useState({
-    email: "rima sardar",
-  });
+  const [user, setUser] = useState(null);
+
+  console.log(user);
+
+  const createUser = (email, password) => {
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+    });
+    return () => {
+      unsubscribe();
+    };
+  }, []);
+
   const authData = {
     user,
     setUser,
+    createUser,
   };
   return <AuthContext value={authData}>{children}</AuthContext>;
 };
